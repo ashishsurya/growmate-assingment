b4a871614c7f8f017814bc58fb59ff3232c27037
ashishsurya <ashishsurya126@gmail.com>
2022-09-21 22:10:08 +0530

    all three tabs completed

---
+++ components/RelatedNewsCard.jsx
@@ -0,0 +1,18 @@
+import { View, Image } from 'react-native';
+import { Text } from '@rneui/base';
+import React from 'react';
+import tw from 'twrnc';
+
+export default function RelatedNewsCard({ cardImageUrl, title }) {
+  return (
+    <View style={tw`flex flex-row border my-2 h-25 rounded-xl items-center`}>
+      <Image
+        style={tw`h-25 w-25 rounded-l-xl`}
+        source={{
+          uri: cardImageUrl,
+        }}
+      />
+      <Text style={{ fontSize: 20, flex: 1,marginLeft:10,color:"gray" }}>{title}</Text>
+    </View>
+  );
+}
--- components/Tabs/Related.jsx
+++ components/Tabs/Related.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text } from 'react-native';
+import React from 'react';
+import RelatedNewsCard from '../RelatedNewsCard';
+import tw from "twrnc"
 export default function Related() {
   return (
-    <View>
-      <Text>Related</Text>
+    <View style={tw`flex p-4`}>
+      <RelatedNewsCard
+        cardImageUrl={
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjkK3uRW156Zzsu4L1_SjcC1iAqTvhaMgyDg&usqp=CAU'
+        }
+        title="Liz Truss will be UK's next Prime Minister ?"
+      />
+      <RelatedNewsCard
+        cardImageUrl={
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjkK3uRW156Zzsu4L1_SjcC1iAqTvhaMgyDg&usqp=CAU'
+        }
+        title="Liz Truss will be UK's next Prime Minister ?"
+      />
+      <Text style={tw`self-end`}>More..</Text>
     </View>
-  )
+  );
 }
